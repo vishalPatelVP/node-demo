37c72a22051f93e558aa95a6e65146d4aa4de0a4
Vishal(VP) <vishal@adit.com>
2020-08-24 10:11:41 +0530

    vp:waring remove mongo connection

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/vpapp', {useNewUrlParser: true})
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
--- controller/user.ctrl.js
+++ controller/user.ctrl.js
@@ -1,5 +1,6 @@
 const User = require('./../models/user');
 const Helper = require('./../commongHelper');
+const mongoose = require('mongoose')
 
 router.post('/createUser',[Helper.checkValidation],(req,res)=>{
     try {
@@ -26,9 +27,11 @@ router.get('/findall',[Helper.checkValidation],async(req,res)=>{
 
 router.get('/find',[Helper.checkValidation],async(req,res)=>{
     try {
-        let user = await User.find({_id:req.query.id});
+        let user = await mongoose.connection.db.collection('users').aggregate([
+            { $match: { _id: req.query.id } }
+        ], { allowDiskUse: true, collation: { locale: 'en_US', alternate: "shifted" } }).toArray();vvvv
         res.send(user);
     } catch (error) {
         return res.send(error);
     }
-});
+})
